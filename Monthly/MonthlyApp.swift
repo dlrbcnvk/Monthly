5f66a7947e4d92c0cdc720d434f12bb23871f7f9
dlrbcnvk <dlrbcnvk@khu.ac.kr>
2022-10-09 18:39:25 +0900

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by 조성규 on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
